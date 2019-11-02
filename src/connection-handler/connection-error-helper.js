bd84a4133782c80ed8e788b50cbd944d7f3a2449
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2019-11-02 19:10:10 +0700

    change connecton alert type

--- src/connection-handler/connection-error-helper.js
+++ src/connection-handler/connection-error-helper.js
@@ -45,12 +45,12 @@ export function setDropDownIntance(instance) {
 
 export function showConnectedWarning() {
   currentAlert = ALERT_TYPE_CONNECTED
-  dropDownInstance.alertWithType('success', GlobalConst.getValue().CONNECTION_SUCCESS_TITLE, GlobalConst.getValue().CONNECTION_SUCCESS_MESSAGE, { subType: 'connection' }, 1000)
+  dropDownInstance.alertWithType(ALERT_TYPE_CONNECTED.type, GlobalConst.getValue().CONNECTION_SUCCESS_TITLE, GlobalConst.getValue().CONNECTION_SUCCESS_MESSAGE, { subType: ALERT_TYPE_CONNECTED.subType }, 1000)
 }
 
 export function showDisconnectedWarning() {
   currentAlert = ALERT_TYPE_DISCONNECTED
-  dropDownInstance.alertWithType('error', GlobalConst.getValue().CONNECTION_ERROR_TITLE, GlobalConst.getValue().CONNECTION_ERROR_MESSAGE, { subType: 'connection' });
+  dropDownInstance.alertWithType(ALERT_TYPE_DISCONNECTED.type, GlobalConst.getValue().CONNECTION_ERROR_TITLE, GlobalConst.getValue().CONNECTION_ERROR_MESSAGE, { subType: ALERT_TYPE_DISCONNECTED.subType });
 }
 
 export function showAlert200(response, callback) {
@@ -58,7 +58,7 @@ export function showAlert200(response, callback) {
   if (callback) {
     callback(response)
   } else {
-    dropDownInstance.alertWithType('success', GlobalConst.getValue().CONNECTION_200_ALERT_TITLE, GlobalConst.getValue().CONNECTION_200_ALERT_MESSAGE, { subType: '200' }, 1000);
+    dropDownInstance.alertWithType(ALERT_TYPE_200.type, GlobalConst.getValue().CONNECTION_200_ALERT_TITLE, GlobalConst.getValue().CONNECTION_200_ALERT_MESSAGE, { subType:ALERT_TYPE_200.subType }, 1000);
   }
 }
 
@@ -68,7 +68,7 @@ export function showAlert400(response, callback) {
     if (callback) {
       callback(response)
     } else {
-      dropDownInstance.alertWithType('error', GlobalConst.getValue().CONNECTION_400_ALERT_TITLE, GlobalConst.getValue().CONNECTION_400_ALERT_MESSAGE, { subType: '400' });
+      dropDownInstance.alertWithType(ALERT_TYPE_400.type, GlobalConst.getValue().CONNECTION_400_ALERT_TITLE, GlobalConst.getValue().CONNECTION_400_ALERT_MESSAGE, { subType: ALERT_TYPE_400.subType });
     }
   }
 }
@@ -79,7 +79,7 @@ export function showAlert500(response, callback) {
     if (callback) {
       callback(response)
     } else {
-      dropDownInstance.alertWithType('error', GlobalConst.getValue().CONNECTION_500_ALERT_TITLE, GlobalConst.getValue().CONNECTION_500_ALERT_MESSAGE, { subType: '500' });
+      dropDownInstance.alertWithType(ALERT_TYPE_500.type, GlobalConst.getValue().CONNECTION_500_ALERT_TITLE, GlobalConst.getValue().CONNECTION_500_ALERT_MESSAGE, { subType: ALERT_TYPE_500.subType });
     }
   }
 }
