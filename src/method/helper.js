dd2fff623ed429ea609eb1e9bd0d7cc1b611bdf0
muhammad iqbal <muhammadiqbalks2@gmail.com>
2019-12-17 00:40:59 +0700

    add other features for custom-select

--- src/method/helper.js
+++ src/method/helper.js
@@ -83,6 +83,10 @@ export function getFileNameFromURL(url) {
   return getFileNameFromPath(pathname)
 }
 
+export function stringEquals(str1, str2) {
+  return str1.toLowerCase() === str2.toLowerCase();
+}
+
 export function isEmptyOrSpaces(str) {
   if (str) {
     return str.match(/^ *$/) !== null;
